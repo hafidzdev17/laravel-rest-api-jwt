d46e0551d012606b00837a54e4c65aafa945d215
hafidzdev17 <do.crazy192@gmail.com>
2023-10-11 22:06:11 +0700

    feat: update route API with post

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\API\PostController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +13,8 @@ Route::post('/login',[\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/refresh',[\App\Http\Controllers\AuthController::class, 'refresh'])
     ->middleware('jwt.verify');
 Route::get('/me',[\App\Http\Controllers\AuthController::class, 'me'])
+    ->middleware('jwt.auth');
+
+// post
+Route::resource('post', PostController::class)
     ->middleware('jwt.auth');
